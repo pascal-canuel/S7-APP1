0e6044a3ccaf5fc3b586d4fed7e2754003c9788b
Pascal Canuel <canp2403@usherbrooke.ca>
2023-01-18 17:11:43 -0500

    add spaces

--- dnn_framework/student/optimizers.py
+++ dnn_framework/student/optimizers.py
@@ -11,4 +11,4 @@ class SgdOptimizer(Optimizer):
         super().__init__(parameters)
 
     def _step_parameter(self, parameter, parameter_grad, parameter_name):
-        return parameter - self.learning_rate*parameter_grad
+        return parameter - self.learning_rate * parameter_grad
